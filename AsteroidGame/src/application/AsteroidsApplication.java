139eeb997564f61922b419ccd632279a94be0c59
mertt11 <mertdeneme10@outlook.com>
2022-03-08 14:19:21 +0300

    AsteroidGame

---
+++ AsteroidGame/src/application/AsteroidsApplication.java
@@ -0,0 +1,201 @@
+package application;
+	
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class AsteroidsApplication extends Application {
+    public static int WIDTH = 300;
+    public static int HEIGHT = 200;
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        Text text = new Text(10, 20, "Points: 0");
+        pane.getChildren().add(text);
+        AtomicInteger points = new AtomicInteger();
+        pane.setPrefSize(WIDTH, HEIGHT);
+        
+        Ship ship = new Ship(WIDTH/2, HEIGHT/2);
+        
+        List<Asteroid> asteroids=new ArrayList<>(); 
+        List<Projectile> projectiles = new ArrayList<>();
+        
+        for(int i=0;i<5;i++) {
+        	Random random=new Random();
+        	Asteroid asteroid=new Asteroid(random.nextInt(WIDTH / 3), random.nextInt(HEIGHT));
+        	asteroids.add(asteroid);
+        }
+        
+        
+        
+        pane.getChildren().add(ship.getCharacter());
+        asteroids.forEach(asteroid -> pane.getChildren().add(asteroid.getCharacter()));
+        
+        asteroids.forEach(asteroid -> asteroid.turnRight());
+        asteroids.forEach(asteroid -> asteroid.accelerate());
+        
+        
+        Scene scene = new Scene(pane);
+        
+        Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
+        
+
+        
+        scene.setOnKeyPressed(event -> {
+            pressedKeys.put(event.getCode(), Boolean.TRUE);
+        });
+        scene.setOnKeyReleased(event -> {
+            pressedKeys.put(event.getCode(), Boolean.FALSE);
+        });
+        
+        new AnimationTimer() {
+            @Override
+            
+            public void handle(long now) {
+
+
+
+                if (pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
+                    ship.turnLeft();
+                }
+
+                if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
+                    ship.turnRight();
+                }
+                
+                if (pressedKeys.getOrDefault(KeyCode.UP, false)) {
+                    ship.accelerate();
+                }
+                
+                if (pressedKeys.getOrDefault(KeyCode.SPACE, false) && projectiles.size() < 3) {
+                    // we shoot
+                    Projectile projectile = new Projectile((int) ship.getCharacter().getTranslateX(), (int) ship.getCharacter().getTranslateY());
+                    projectile.getCharacter().setRotate(ship.getCharacter().getRotate());
+                    projectiles.add(projectile);
+                    
+                    projectile.accelerate();
+                    projectile.setMovement(projectile.getMovement().normalize().multiply(3));
+                    
+                    pane.getChildren().add(projectile.getCharacter());
+                }
+                
+                ship.move();
+                asteroids.forEach(asteroid -> asteroid.move());
+                projectiles.forEach(projectile -> projectile.move());
+
+                asteroids.forEach(asteroid -> {
+                    if (ship.collide(asteroid)) {
+                        stop();
+                    }
+                });
+                projectiles.forEach(projectile -> {
+                    asteroids.forEach(asteroid -> {
+                        if (projectile.collide(asteroid)) {
+                            projectile.setAlive(false);
+                            asteroid.setAlive(false);
+                        }
+                    });
+                    
+                    if (!projectile.isAlive()) {
+                        text.setText("Points: " + points.addAndGet(1000));
+                    }
+                });
+
+                      
+                projectiles.stream()
+                .filter(projectile -> !projectile.isAlive())
+                .forEach(projectile -> pane.getChildren().remove(projectile.getCharacter()));
+            
+            projectiles.removeAll(projectiles.stream()
+                .filter(projectile -> !projectile.isAlive())
+                .collect(Collectors.toList()));
+
+            asteroids.stream()
+                .filter(asteroid -> !asteroid.isAlive())
+                .forEach(asteroid -> pane.getChildren().remove(asteroid.getCharacter()));
+
+            asteroids.removeAll(asteroids.stream()
+                .filter(asteroid -> !asteroid.isAlive())
+                .collect(Collectors.toList()));
+                
+            
+                if(Math.random() < 0.01) {
+                    Asteroid asteroid = new Asteroid(WIDTH, HEIGHT);
+                    if (!asteroid.collide(ship)) {
+                        asteroids.add(asteroid);
+                        pane.getChildren().add(asteroid.getCharacter());
+                    }
+                }
+                
+            }
+        }.start();
+        
+        stage.setTitle("Asteroids!"); 
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
---
+++ AsteroidGame/src/application/Projectile.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.scene.shape.Polygon;
+
+public class Projectile extends Character {
+
+    public Projectile(int x, int y) {
+        super(new Polygon(2, -2, 2, 2, -2, 2, -2, -2), x, y);
+    }
+
+}
